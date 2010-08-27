277a511ad4cce3ca6d6d1ce6a49f12f79334822f
agentzh (章亦春) <agentzh@gmail.com>
2010-08-27 15:00:12 +0800

    prevent rebuilding in build.sh.

--- util/build.sh
+++ util/build.sh
@@ -23,9 +23,9 @@ if [ ! -s "nginx-$version.tar.gz" ]; then
     fi
 fi
 
-tar -xzvf nginx-$version.tar.gz || exit 1
-cp $root/../no-pool-nginx/nginx-0.8.41-no_pool.patch ./
-patch -p0 < nginx-0.8.41-no_pool.patch
+#tar -xzvf nginx-$version.tar.gz || exit 1
+#cp $root/../no-pool-nginx/nginx-0.8.41-no_pool.patch ./
+#patch -p0 < nginx-0.8.41-no_pool.patch
 
 cd nginx-$version/
 if [[ "$BUILD_CLEAN" -eq 1 || ! -f Makefile || "$root/config" -nt Makefile || "$root/util/build.sh" -nt Makefile ]]; then
